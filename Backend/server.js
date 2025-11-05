28830dce593b50948f92832709ebc72884c40b1f
Enigma-Deez <cosmosjay21@gmail.com>
2025-11-05 13:29:51 +0100

    Checkpoint for host testing

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,16 @@ import scoreRoutes from "./Routes/scoreRoutes.js";
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "https://typa-iota.vercel.app"
+  ],
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/scores", scoreRoutes);
